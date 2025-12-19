cf6d32b3389c2eca6e878b44163b6f649e0aae63
birukshegute <birukshegute@gmail.com>
2025-12-19 11:49:30 +0300

    admin.py update 1

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Note
 
-# Register your models here.
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "owner", "is_encrypted", "created_at")
+    list_filter = ("is_encrypted", "created_at")
+    search_fields = ("title", "content")
+    readonly_fields = ("created_at", "updated_at")
